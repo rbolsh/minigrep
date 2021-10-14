a0cf4d1feeeb443296b6b6069f42ae589ca677cf
Roman Bolshakov <r.bolshakov@iml.ru>
2021-10-15 00:40:50 +0300

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config{
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Self, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments provided");
+        }
+
+        Ok(
+            Self { 
+                query: args[1].clone(),
+                filename: args[2].clone(),
+            }
+        )
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+
