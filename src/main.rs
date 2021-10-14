ef97cd27ae81fc5e52ce96d0fe096216f220db2c
Roman Bolshakov <r.bolshakov@iml.ru>
2021-10-14 17:45:16 +0300

    Create Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
+
     println!("{:?}", args);
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+
+struct Config{
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self{
+//    let query = args[1].clone();
+  //  let filename = args[2].clone();
+
+        Self { 
+            query: args[1].clone(),
+            filename: args[2].clone(),
+            }
+}
 }
